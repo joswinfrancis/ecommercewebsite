c0dfba46adb7fcb8bb17ebedf59d8f40fc361f54
Joswin Francis <joswinfrancis2001@gmail.com>
2024-05-22 16:29:08 +0530

    set all page layouts logo changed nav links and urls set

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,12 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index , name="index"),
+    path('products/',views.products , name='products'),
+    path('about/',views.about , name='about'),
+    path('contact/',views.contact , name='contact'),
+    path('blog_list/',views.blog_list , name='blog_list'),
+    path('testimonials/',views.testimonials , name='testimonials'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def products(request):
+    return render(request,'product.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def blog_list(request):
+    return render(request,'blog_list.html')
+
+
+def testimonials(request):
+    return render(request,'testimonial.html')
