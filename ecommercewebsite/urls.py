34fd341f1c75fe5bd40e6efe3480aabccf6043f7
Joswin Francis <joswinfrancis2001@gmail.com>
2024-05-27 14:44:56 +0530

    edited all pages header and footer created carts app and set cart functions

--- ecommercewebsite/urls.py
+++ ecommercewebsite/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('products.urls'))
+    path('',include('products.urls')),
+    path('cart/',include('orders.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
